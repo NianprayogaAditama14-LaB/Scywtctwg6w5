8d356647c11d7ce9a72dd2b17f5d4a61b1bcfb4f
CodeSanzz <mlus01089@gmail.com>
2026-03-01 03:45:42 +0700

    Remove old providers and add FilmKita

---
+++ FilmKita/src/main/kotlin/com/filmkita/FilmKitaPlugin.kt
@@ -0,0 +1,13 @@
+package com.dutamovie
+
+import android.content.Context
+import com.lagradost.cloudstream3.plugins.CloudstreamPlugin
+import com.lagradost.cloudstream3.plugins.Plugin
+
+@CloudstreamPlugin
+class DutaMoviePlugin : Plugin() {
+    override fun load(context: Context) {
+        DutaMovie.context = context
+        registerMainAPI(FilmKita())
+    }
+}
