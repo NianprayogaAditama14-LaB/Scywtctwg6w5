4ee2b46cc877ba502165d7ab4e517010a878ba9b
NianprayogaAditama14-LaB <zinanprayogaaditama14@gmail.com>
2026-03-01 12:23:37 +0700

    Update FilmKitaPlugin.kt

--- FilmKita/src/main/kotlin/com/filmkita/FilmKitaPlugin.kt
+++ FilmKita/src/main/kotlin/com/filmkita/FilmKitaPlugin.kt
@@ -1,12 +1,14 @@
 
 package com.filmkita
 
+import android.content.Context
 import com.lagradost.cloudstream3.plugins.CloudstreamPlugin
 import com.lagradost.cloudstream3.plugins.Plugin
 
 @CloudstreamPlugin
 class FilmKitaPlugin : Plugin() {
-    override fun load() {
+    override fun load(context: Context) {
         registerMainAPI(FilmKita())
+        registerExtractorAPI(LayarWibuExtractor())
     }
 }
