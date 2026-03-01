c799ce06b0ab3090abaa65e6e88e2f67f7ec6405
NianprayogaAditama14-LaB <zinanprayogaaditama14@gmail.com>
2026-03-01 11:48:58 +0700

    Update FilmKitaPlugin.kt

--- FilmKita/src/main/kotlin/com/filmkita/FilmKitaPlugin.kt
+++ FilmKita/src/main/kotlin/com/filmkita/FilmKitaPlugin.kt
@@ -1,13 +1,12 @@
+
 package com.filmkita
 
-import android.content.Context
 import com.lagradost.cloudstream3.plugins.CloudstreamPlugin
 import com.lagradost.cloudstream3.plugins.Plugin
 
 @CloudstreamPlugin
 class FilmKitaPlugin : Plugin() {
-    override fun load(context: Context) {
-        DutaMovie.context = context
+    override fun load() {
         registerMainAPI(FilmKita())
     }
 }
