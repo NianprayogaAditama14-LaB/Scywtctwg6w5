f7bcc6c13f6f6138a3e966f3916cf2dcf415d5ce
NianprayogaAditama14-LaB <zinanprayogaaditama14@gmail.com>
2026-03-01 12:41:44 +0700

    Update Extractors.kt

--- FilmKita/src/main/kotlin/com/filmkita/Extractors.kt
+++ FilmKita/src/main/kotlin/com/filmkita/Extractors.kt
@@ -4,7 +4,6 @@ import android.util.Base64
 import com.lagradost.cloudstream3.utils.ExtractorApi
 import com.lagradost.cloudstream3.utils.ExtractorLink
 import com.lagradost.cloudstream3.utils.Qualities
-import com.lagradost.cloudstream3.utils.SubtitleFile
 
 class LayarWibuExtractor : ExtractorApi() {
 
@@ -14,21 +13,20 @@ class LayarWibuExtractor : ExtractorApi() {
 
     override suspend fun getUrl(
         url: String,
-        referer: String?,
-        subtitleCallback: (SubtitleFile) -> Unit,
-        callback: (ExtractorLink) -> Unit
-    ) {
+        referer: String?
+    ): List<ExtractorLink>? {
+
         val encoded = url.substringAfterLast("/")
         val decoded = String(Base64.decode(encoded, Base64.DEFAULT))
 
-        callback.invoke(
+        return listOf(
             ExtractorLink(
-                name,
-                name,
-                decoded,
-                mainUrl,
-                Qualities.P1080.value,
-                true
+                source = name,
+                name = name,
+                url = decoded,
+                referer = "",
+                quality = Qualities.P1080.value,
+                isM3u8 = true
             )
         )
     }
