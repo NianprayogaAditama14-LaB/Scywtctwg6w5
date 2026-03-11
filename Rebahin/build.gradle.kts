d759d05239bb0a06f8f73bd99dc51e1389ac9a4c
CodeSanzz <mlus01089@gmail.com>
2026-03-12 01:30:07 +0700

    Add Rebahin provider

---
+++ Rebahin/build.gradle.kts
@@ -0,0 +1,24 @@
+version = 1
+
+cloudstream {
+    description = "Rebahin"
+    language = "id"
+    authors = listOf("Sanzz")
+
+    /**
+     * Status int as the following:
+     * 0: Down
+     * 1: Ok
+     * 2: Slow
+     * 3: Beta only
+     * */
+    status = 1
+
+    tvTypes = listOf(
+        "AsianDrama",
+        "TvSeries",
+        "Movie",
+    )
+
+    iconUrl = "https://wiapr.com/favicon.ico"
+}
---
+++ Rebahin/src/main/kotlin/com/rebahin/RebahinPlugin.kt
@@ -0,0 +1,13 @@
+package com.rebahin
+
+import android.content.Context
+import com.lagradost.cloudstream3.plugins.CloudstreamPlugin
+import com.lagradost.cloudstream3.plugins.Plugin
+
+@CloudstreamPlugin
+class RebahinPlugin : Plugin() {
+    override fun load(context: Context) {
+        Kawanfilm.context = context
+        registerMainAPI(Rebahin())
+    }
+}
